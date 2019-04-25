4c20d73bcbd283ca9a3fe6a5ceffb44af51d312b
Tom O <thomasowca@yahoo.com>
2019-04-24 23:08:23 -0500

    Add IOrderHistory.cs interface. Started to build up the OrderHistory.cs service layer class

---
+++ TamsPizzeriaWebApp/TamsPizzeriaWebApp/Services/IOrderHistory.cs
@@ -0,0 +1,36 @@
+﻿using PizzeriaData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TamsPizzeriaWebApp.Services
+{
+    public interface IOrderHistory
+    {
+        // Different varieties on getting a list of orders at the business.
+        IEnumerable<Order> GetAllOrdersByDefault();
+        IEnumerable<Order> GetAllOrdersByEmployee(int id);
+        IEnumerable<Order> GetAllOrdersByCustomerLastName(string lastName);
+        IEnumerable<Order> GetAllOrdersByStatus(int id);
+        IEnumerable<Order> GetAllOrdersByPizzaId(int id);
+        IEnumerable<Order> GetAllOrdersByStorePickupId(int id);
+
+        // Two different ways to get one specific order from history.
+        Order GetOrderByConfirmation(int confirmation);
+        Order GetOrderById(int id);
+
+        // Delete and update operations for existing orders in history.
+        void DeleteOrderById(int id);
+        void DeleteOrderByConfirmation(int id);
+        void UpdateOrder(Order order);
+
+        // Get various business statistics for managers/admins.
+        decimal GetTotalRevenueBySubTotal();
+        decimal GetTotalRevenueByFinalTotal();
+
+        int GetTotalOrdersCreatedAtCompany();
+        int GetTotalOrdersCreatedAtStore(int id);
+        int GetTopCrust();
+    }
+}
---
+++ TamsPizzeriaWebApp/TamsPizzeriaWebApp/Services/OrderHistory.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PizzeriaData.Models;
+using TamsPizzeriaWebApp.Data;
+
+namespace TamsPizzeriaWebApp.Services
+{
+    // TO-DO | Finish the implementation of all these service layer methods! 
+    public class OrderHistory : IOrderHistory
+    {
+        private ApplicationDbContext _context;
+
+        public OrderHistory(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteOrderByConfirmation(int confirmation)
+        {
+            var order = _context.Orders
+                .FirstOrDefault(o => o.Confirmation == confirmation);
+
+            _context.Remove(order);
+            _context.SaveChanges();
+        }
+
+        public void DeleteOrderById(int id)
+        {
+            var order = _context.Orders
+                .FirstOrDefault(o => o.Id == id);
+
+            _context.Remove(order);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Order> GetAllOrdersByCustomerLastName(string lastName)
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .Where(o => o.LastName == lastName);
+        }
+
+        public IEnumerable<Order> GetAllOrdersByDefault()
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .OrderByDescending(o => o.OrderDate);
+        }
+
+        public IEnumerable<Order> GetAllOrdersByEmployee(int id)
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .Where(o => o.FulFilledById == id);
+        }
+
+        public IEnumerable<Order> GetAllOrdersByPizzaId(int id)
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .Where(o => o.Pizza.Id == id);
+        }
+
+        public IEnumerable<Order> GetAllOrdersByStatus(int id)
+        {
+            return _context.Orders
+               .Include(o => o.Status)
+               .Include(o => o.Pizza)
+               .Include(o => o.StorePickup)
+               .Where(o => o.Status.Id == id);
+        }
+
+        public IEnumerable<Order> GetAllOrdersByStorePickupId(int id)
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .Where(o => o.StorePickup.Id == id);
+        }
+
+        public Order GetOrderByConfirmation(int confirmation)
+        {
+            return _context.Orders
+                .Include(o => o.Status)
+                .Include(o => o.Pizza)
+                .Include(o => o.StorePickup)
+                .FirstOrDefault(o => o.Confirmation == confirmation);
+        }
+
+        public Order GetOrderById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTopCrust()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalOrdersCreatedAtCompany()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalOrdersCreatedAtStore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTotalRevenueByFinalTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetTotalRevenueBySubTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
